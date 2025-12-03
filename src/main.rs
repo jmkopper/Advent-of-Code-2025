ed1ea5078d3a471bb7c8f5db736f9dbc60a5a344
Jay Kopper <jkopper@enova.com>
2025-12-03 09:41:12 -0500

    day 3

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,11 @@ use std::error::Error;
 mod solutions;
 
 fn main() -> Result<(), Box<dyn Error>> {
+    println!("---- Day 1 ----");
     solutions::day01::run()?;
+    println!("---- Day 2 ----");
     solutions::day02::run()?;
+    println!("---- Day 3 ----");
+    solutions::day03::run()?;
     Ok(())
 }
